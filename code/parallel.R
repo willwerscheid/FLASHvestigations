5e83c3ebd7b92e2a08f49066b44a1dd605f4cdb6
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-08-10 10:13:22 -0500

    add parallel code

---
+++ code/parallel.R
@@ -0,0 +1,100 @@
+flash_update_fl_parallel = function(data,
+                                    f,
+                                    kset,
+                                    var_type,
+                                    ebnm_fn_l,
+                                    ebnm_param_l,
+                                    ebnm_fn_f,
+                                    ebnm_param_f) {
+  f = flash_update_precision(data, f, var_type)
+  f = flash_update_loadings_parallel(data,
+                                     f,
+                                     kset,
+                                     ebnm_fn_l,
+                                     ebnm_param_l)
+  f = flash_update_factors_parallel(data,
+                                    f,
+                                    kset,
+                                    ebnm_fn_f,
+                                    ebnm_param_f)
+}
+
+
+flash_update_loadings_parallel = function(data,
+                                          f,
+                                          kset,
+                                          ebnm_fn,
+                                          ebnm_param) {
+  R = flash_get_R(data, f)
+  subset = !f$fixl
+
+  update_fn = function(k) {
+    Rk = R + outer(f$EL[, k], f$EF[, k])
+    calc_update_vals(data,
+                     f,
+                     k,
+                     which(subset[, k]),
+                     ebnm_fn[[k]],
+                     ebnm_param[[k]],
+                     loadings = TRUE,
+                     Rk)
+  }
+  res = lapply(as.list(kset), update_fn)
+
+  # Deal with "failed" updates:
+  null_idx = which(sapply(res, is.null))
+  if (length(null_idx) > 0) {
+    res = res[-null_idx]
+    kset = kset[-null_idx]
+  }
+  subset[, -kset] = FALSE
+
+  f$EL[subset] = unlist(lapply(res, function(k) {k$EX}))
+  f$EL2[subset] = unlist(lapply(res, function(k) {k$EX2}))
+  f$ebnm_fn_l[kset] = ebnm_fn[kset]
+  f$ebnm_param_l[kset] = ebnm_param[kset]
+  f$gl[kset] = lapply(res, function(k) {k$g})
+  f$KL_l[kset] = lapply(res, function(k) {k$KL})
+
+  return(f)
+}
+
+
+flash_update_factors_parallel = function(data,
+                                         f,
+                                         kset,
+                                         ebnm_fn,
+                                         ebnm_param) {
+  R = flash_get_R(data, f)
+  subset = !f$fixf
+
+  update_fn = function(k) {
+    Rk = R + outer(f$EL[, k], f$EF[, k])
+    calc_update_vals(data,
+                     f,
+                     k,
+                     which(subset[, k]),
+                     ebnm_fn[[k]],
+                     ebnm_param[[k]],
+                     loadings = FALSE,
+                     Rk)
+  }
+  res = lapply(as.list(kset), update_fn)
+
+  # Deal with "failed" updates:
+  null_idx = which(sapply(res, is.null))
+  if (length(null_idx) > 0) {
+    res = res[-null_idx]
+    kset = kset[-null_idx]
+  }
+
+  subset[, -kset] = FALSE
+  f$EF[subset] = unlist(lapply(res, function(k) {k$EX}))
+  f$EF2[subset] = unlist(lapply(res, function(k) {k$EX2}))
+  f$ebnm_fn_f[kset] = ebnm_fn[kset]
+  f$ebnm_param_f[kset] = ebnm_param[kset]
+  f$gf[kset] = lapply(res, function(k) {k$g})
+  f$KL_f[kset] = lapply(res, function(k) {k$KL})
+
+  return(f)
+}
---
+++ code/parallel_test.R
@@ -0,0 +1,62 @@
+niter <- 40
+nfactors <- 20
+
+# devtools::install_github("stephenslab/flashr")
+devtools::load_all("/Users/willwerscheid/GitHub/flashr/")
+# devtools::install_github("stephenslab/ebnm")
+devtools::load_all("/Users/willwerscheid/GitHub/ebnm/")
+
+source("./code/parallel.R")
+
+gtex <- readRDS(gzcon(url("https://github.com/stephenslab/gtexresults/blob/master/data/MatrixEQTLSumStats.Portable.Z.rds?raw=TRUE")))
+
+strong <- t(gtex$strong.z)
+strong_data <- flash_set_data(strong, S = 1)
+
+fl <- flash_add_greedy(strong_data, nfactors, var_type = "zero")
+fl_greedy <- fl
+
+fl <- fl_greedy # load
+backfit_t <- rep(0, niter)
+backfit_obj <- rep(0, niter)
+for (i in 1:niter) {
+   t <- system.time(
+    for (k in 1:nfactors) {
+      fl <- flashr:::flash_update_single_fl(strong_data,
+                                            fl,
+                                            k,
+                                            "zero",
+                                            "ebnm_pn", list(),
+                                            "ebnm_pn", list())
+    }
+  )
+  backfit_t[i] <- t[3] # elapsed time
+  backfit_obj[i] <- flash_get_objective(strong_data, fl)
+}
+
+fl <- fl_greedy
+parallel_t <- rep(0, niter)
+parallel_obj <- rep(0, niter)
+ebnm_param <- vector("list", nfactors)
+for (k in 1:nfactors) {ebnm_param[[k]] <- list()}
+for (i in 1:niter) {
+  t <- system.time(
+    fl <- flash_update_fl_parallel(strong_data,
+                                 fl,
+                                 1:nfactors,
+                                 "zero",
+                                 as.list(rep("ebnm_pn", nfactors)),
+                                 ebnm_param,
+                                 as.list(rep("ebnm_pn", nfactors)),
+                                 ebnm_param)
+  )
+  parallel_t[i] <- t[3]
+  parallel_obj[i] <- flash_get_objective(strong_data, fl)
+}
+
+plot(backfit_obj, pch=1)
+points(parallel_obj, pch=2)
+plot(backfit_obj - parallel_obj, type = "l")
+
+boxplot(cbind(backfit_t, parallel_t))
+c(sum(backfit_t), sum(parallel_t))
